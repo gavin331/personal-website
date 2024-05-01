9ff222722971581a3b0b31fec9ae2501807ae860
gavin331 <e0957939@u.nus.edu>
2024-05-01 18:20:40 +0800

    Add top navigation bar

---
+++ personal website/src/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+	return(
+        <>
+            <nav>
+                <h1>Gavin Goh J. W.</h1>
+                <ul>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Experiences</a></li>
+                    <li><a href="">Projects</a></li>
+                    <li><a href="">Contacts</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+export default Header
